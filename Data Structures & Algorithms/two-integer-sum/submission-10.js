c011fb617abe007dc7e7cb442693ad9e355516f5
Lokesh Nandan Meher <lokeshmeher34@gmail.com>
2026-08-10 14:25:38 +0530

    Add: two-integer-sum - submission-10

---
+++ Data Structures & Algorithms/two-integer-sum/submission-10.js
@@ -0,0 +1,73 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+        /**
+         * i + j = target
+         * for each i, check if target-i / j exists --> we can do this part in constant time
+         * by using a hash map of values in num to the index at which that value occurs.
+         * --> EDGE CASE: duplicate values => if (i === j) then store indices of i and j
+         * in an array and return the first 2 values from that array -- the array will contain
+         * unique indices.
+         * when you find it, return the indices of i and j.
+         */
+        /**
+         * 1. first create the hash map of values to indices, if value exists then save an array.
+         * 2. Loop over each element in the hash map ->[i]-> (since hash maps can be looped over in
+         * insertion order) and find the index of the other ->[j].
+         * 3. If i==j, then first check if at least two distinct values exists in the hash map for
+         * that key, then return them.
+         * 
+         * Time: O(n), Space: O(n) - where n is the size of the nums array.
+         */
+        // let indexMap = new Map();
+        // for (let [i, num] of nums.entries()) {
+        //     if (indexMap.has(num)) {
+        //         indexMap.set(num, [...indexMap.get(num), i]);
+        //     } else {
+        //         indexMap.set(num, [i]);
+        //     }
+        // }
+
+        // // WE can loop over `indexMap` instead of `nums` because javascript Map saves insertion order.
+        // for (let [num, idx] of indexMap) {
+        //     let other = indexMap.get(target-num);
+        //     if (other === undefined) continue;
+            
+        //     if (num === target-num) {
+        //         // If pair/more of duplicates exists, for e.g. [5, 5] or [1, 3, 3, 5].
+        //         if (idx.length > 1) {
+        //             return [idx[0], idx[1]];
+        //         }
+        //     }
+        //     else {
+        //         return [idx[0], other[0]];
+        //     }
+        // }
+
+        /**
+         * There is a simpler way to do it since in the problem statement we are given that 
+         * there will be exactly one pair of indices that satisfy the condition.
+         * 
+         * We can create the hashmap and check for the value in the same iteration.
+         * Below code demonstrates this.
+         */
+        let valueToIndex = new Map();
+        for (let [index, num] of nums.entries()) {
+            if (valueToIndex.has(target-num)) {
+                return [valueToIndex.get(target-num), index]
+            }
+            valueToIndex.set(num, index);
+        }
+
+        /**
+         * A semi optimized solution could use a list of pairs (number to it's index) instead of a
+         * hashmap, e.g. [[3, 0], [4, 1], [5, 2], [6, 3]]. THen we can sort the numbers and use 
+         * two pointers (one at the beginning, one at the end of the list) to find the numbers whose
+         * sum equals target. TIme would be O(nlogn) and space will be O(n).
+         */
+    }
+}
