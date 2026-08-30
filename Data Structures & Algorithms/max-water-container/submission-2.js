592db308a1b1d1deeab15dc9d80217cbda22db47
Lokesh Nandan Meher <lokeshmeher34@gmail.com>
2026-08-30 12:46:57 +0530

    Add: max-water-container - submission-2

---
+++ Data Structures & Algorithms/max-water-container/submission-2.js
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {number[]} heights
+     * @return {number}
+     */
+    maxArea(heights) {
+        /**
+         * Brute force would be to take every combination of two pointers
+         * and calculate the water contained by them.
+         * Time: O(n^2)
+         * Space: O(1)
+         */
+        /**
+         * Optimal approach would be to use two pointers and 
+         */
+        let l = 0, r = heights.length - 1;
+        let maxWater = 0;
+        while (l < r) {
+            let area = (r - l) * Math.min(heights[l], heights[r]);
+            maxWater = Math.max(maxWater, area);
+            if (heights[l] < heights[r]) {
+                l++;
+            }
+            else {
+                r--;
+            }
+        }
+        return maxWater;
+    }
+}
